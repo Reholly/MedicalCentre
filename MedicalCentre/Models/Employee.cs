f83471e474ad8afddb008d2178de90ac3c70a492
Степан <@LAPTOP-B0PF4KGO>
2023-02-07 20:54:32 +0500

    Added Models

--- MedicalCentre/Models/Employee.cs
+++ MedicalCentre/Models/Employee.cs
@@ -7,7 +7,8 @@ namespace MedicalCentre.Models
         public int Id { get; private set; }
         public string Name { get; private set; }
         public string Surname { get; private set; }
-        public string Patronymic { get; private set; }
+        public string? Patronymic { get; private set; }
         public IRole Role { get; private set; }
+        public Specialization Specialization { get; private set; }
     }
 }
---
+++ MedicalCentre/Models/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalCentre.Models
+{
+    public class Patient
+    {
+        public string PhoneNumber { get; }
+        public uint ID { get; }
+        public string Name { get; }
+        public string Surname { get; }
+        public string? Patronymic { get; }
+        public DateOnly BirthDate { get; }
+        public List<MedicalExamination>? Examinations { get; private set; }
+        public List<Note>? Notes { get; private set; }
+    }
+}
