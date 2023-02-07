b8c239c73b90d9f1e61e7b921ef2be1524371cdf
Степан <@LAPTOP-B0PF4KGO>
2023-02-07 20:54:32 +0500

    Added Models

---
+++ MedicalCentre/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MedicalCentre.Models
+{
+    public class Note
+    {
+        public uint ID { get; }
+        public string Title { get; }
+        public string NoteText { get; }
+        public DateTime PublicationDate { get; }
+    }
+}
---
+++ MedicalCentre/Models/Specialization.cs
@@ -0,0 +1,9 @@
+﻿namespace MedicalCentre.Models
+{
+    public class Specialization
+    {
+        public uint ID { get; }
+        public string Title { get; }
+        public uint Salary { get; }
+    }
+}
