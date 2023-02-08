27fb030ec453e6406a53458bb4ddcdfa303e5130
EuUst <atokadota2@yandex.ru>
2023-02-08 19:53:54 +0500

    refactor

--- MedicalCentre/Models/ApplicationContext.cs
+++ MedicalCentre/Models/ApplicationContext.cs
@@ -5,17 +5,17 @@ namespace MedicalCentre.Models
 {
     public class ApplicationContext : DbContext
     {
-        private DbSet<Employee> employees = null!;
-        private DbSet<Log> logs = null!;
-        private DbSet<MedicalExamination> medicalExaminations = null!;
-        private DbSet<Note> notes = null!;
-        private DbSet<Patient> patiens = null!;
-        private DbSet<IRole> roles = null!;
-        private DbSet<Specialization> specializations = null!;       
+        private DbSet<Employee> employees { get; set; }
+        private DbSet<Log> logs { get; set; }
+        private DbSet<MedicalExamination> medicalExaminations { get; set; }
+        private DbSet<Note> notes { get; set; }
+        private DbSet<Patient> patiens { get; set; }
+        private DbSet<IRole> roles { get; set; }
+        private DbSet<Specialization> specializations { get; set; }
 
         public ApplicationContext()
         {
-            Database.EnsureCreatedAsync();
+            
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
