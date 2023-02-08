2919f0fcd8acfc5827cc3d69783ba4b037cd1596
EuUst <atokadota2@yandex.ru>
2023-02-08 17:55:48 +0500

    another refacto before migration

---
+++ MedicalCentre/Models/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using MedicalCentre.Roles;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalCentre.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        private DbSet<Employee> employees = null!;
+        private DbSet<Log> logs = null!;
+        private DbSet<MedicalExamination> medicalExaminations = null!;
+        private DbSet<Note> notes = null!;
+        private DbSet<Patient> patiens = null!;
+        private DbSet<IRole> roles = null!;
+        private DbSet<Specialization> specializations = null!;       
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreatedAsync();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string databaseSettings = Properties.Settings.Default.DatabaseSettings;
+            optionsBuilder.UseMySql(databaseSettings, ServerVersion.AutoDetect(databaseSettings));
+        }
+    }
+}
