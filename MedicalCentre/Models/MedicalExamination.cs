7b8449d417789d49d0104c059f8b8acf85e1b015
EuUst <atokadota2@yandex.ru>
2023-02-09 17:28:51 +0500

    added migrations, MySQL database has been created by that Adding ApllicationContext and DatabaseInteraction, edit Models and Roles

--- MedicalCentre/Models/MedicalExamination.cs
+++ MedicalCentre/Models/MedicalExamination.cs
@@ -3,8 +3,18 @@
     public class MedicalExamination
     {
         public uint Id { get; set; }
+        public uint PatientId { get; set; }
         public string Title { get; set; } = null!;
         public string Conclusion { get; set; } = null!;
-        public Image[] MaterialsImages { get; set; } = null!;
+        public Image AttachedImage { get; set; } = null!;
+
+        public MedicalExamination(uint id, uint patientId, string title, string conclusion, Image attachedImage)
+        {
+            Id = id;
+            PatientId = patientId;
+            Title = title;
+            Conclusion = conclusion;
+            AttachedImage = attachedImage;
+        }
     }
 }
--- MedicalCentre/Models/Specialization.cs
+++ MedicalCentre/Models/Specialization.cs
@@ -5,5 +5,12 @@
         public uint Id { get; set; }
         public string Title { get; set; } = null!;
         public uint Salary { get; set; }
+
+        public Specialization(uint id, string title, uint salary)
+        {
+            Id = id;
+            Title = title;
+            Salary = salary;
+        }
     }
 }
