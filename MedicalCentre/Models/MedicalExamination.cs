f227c654c9f89d490b8531f66bb37f7989fa6a37
Степан <@LAPTOP-B0PF4KGO>
2023-02-12 21:27:34 +0500

    Redacted Medical Examination

--- MedicalCentre/Models/MedicalExamination.cs
+++ MedicalCentre/Models/MedicalExamination.cs
@@ -6,22 +6,22 @@ namespace MedicalCentre.Models
 {
     public class MedicalExamination : INotifyPropertyChanged
     {
-        public uint Id { get; set; }
-        public DateTime ExaminationDate { get; set; }
-        public uint PatientId { get; set; }
-        public Patient Patient { get; set; }
+        public uint Id { get; set; } = default!;
+        public DateTime ExaminationDate { get; set; } = default!;
+        public Patient Patient { get; set; } = null!;
         public string Title { get; set; } = null!;
         public string Conclusion { get; set; } = null!;
-        public Log AttachedImage { get; set; } = null!;
+        public Image AttachedImage { get; set; } = null!;
 
         public MedicalExamination() { }
 
-        public MedicalExamination(Patient patient, string title, string conclusion, Log attachedImage)
+        public MedicalExamination(Patient patient, string title, string conclusion, Image attachedImage, DateTime date)
         {
             Patient = patient;
             Title = title;
             Conclusion = conclusion;
             AttachedImage = attachedImage;
+            ExaminationDate = date;
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
