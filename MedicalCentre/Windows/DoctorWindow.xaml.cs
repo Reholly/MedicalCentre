7743dba985144505eea69ee7b78b54653745128a
Степан <@LAPTOP-B0PF4KGO>
2023-02-16 20:51:58 +0500

    Added Doctor Window (pre-alpha version)

--- MedicalCentre/Windows/DoctorWindow.xaml.cs
+++ MedicalCentre/Windows/DoctorWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using MedicalCentre.ViewModels;
 
 namespace MedicalCentre.Windows
 {
@@ -19,6 +8,7 @@ namespace MedicalCentre.Windows
         public DoctorWindow()
         {
             InitializeComponent();
+            DataContext = new DoctorViewModel(this);
         }
     }
 }
