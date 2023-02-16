69c9198ad181c16c074bb630b87d2ebcd610dfe8
Степан <@LAPTOP-B0PF4KGO>
2023-02-16 20:51:58 +0500

    Added Doctor Window (pre-alpha version)

---
+++ MedicalCentre/ViewModels/DoctorViewModel.cs
@@ -0,0 +1,35 @@
+﻿using MedicalCentre.Models;
+using MedicalCentre.Windows;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MedicalCentre.ViewModels
+{
+    public class DoctorViewModel
+    {
+        public ObservableCollection<MedicalExamination> Examinations { get; set; } = new ObservableCollection<MedicalExamination>();
+        public MedicalExamination SelectedExamination { get; set; }
+        public ICommand AddRowCommand { get; set; }
+        public ICommand ShowInputHelpCommand { get; set; }
+        public ICommand CreatePatientComand { get; set; }
+        private DoctorWindow window;
+
+        public DoctorViewModel(DoctorWindow window)
+        {
+            window = window;
+            AddRowCommand = new RelayCommand(AddRow);
+            ShowInputHelpCommand = new RelayCommand(ShowInputHelp);
+            CreatePatientComand = new RelayCommand(CreatePatient);
+        }
+
+        private void AddRow() => Examinations.Add(new MedicalExamination(new Patient(), "Test", "", null, DateTime.Now));
+        private void ShowInputHelp() => MessageBox.Show("DateTime input foramt: MM/DD/YYYY HH:MM:SS AM (or PM)");
+        private void CreatePatient()
+        {
+            window.patients.Visibility = Visibility.Visible;
+            window.addPatientButton.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ MedicalCentre/ViewModels/RelayCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MedicalCentre.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private Action action;
+        public RelayCommand(Action action) => this.action = action;
+        public bool CanExecute(object parametr) => true;
+#pragma warning disable CS0067
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore CS0067
+        public void Execute(object parametr) => action();
+    }
+}
