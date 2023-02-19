a9068ea2374e4bbe7f91bdca058a019945a5f31c
Степан <@LAPTOP-B0PF4KGO>
2023-02-19 12:04:29 +0500

    Some Refactoring

--- MedicalCentre/ViewModels/DoctorWindowPagesViewModels/AllPatientsPageViewModel.cs
+++ MedicalCentre/ViewModels/DoctorWindowPagesViewModels/AllPatientsPageViewModel.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MedicalCentre.Models;
+using MedicalCentre.Pages.DoctorWindowPages;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Navigation;
-using MedicalCentre.Models;
-using MedicalCentre.Services;
 
 namespace MedicalCentre.ViewModels.DoctorWindowPagesViewModels
 {
@@ -30,7 +25,7 @@ namespace MedicalCentre.ViewModels.DoctorWindowPagesViewModels
         private void ShowPatientInfo()
         {
             NavigationWindow window = new NavigationWindow();
-            window.Content = new object();
+            window.Content = new PatientInfoPage(SelectedPatient);
             window.Show();
         }
     }
