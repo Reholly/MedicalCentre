f06c8131a734521d9cc942d3c37f1ef490f95b60
Степан <@LAPTOP-B0PF4KGO>
2023-02-19 12:04:29 +0500

    Some Refactoring

--- MedicalCentre/ViewModels/DoctorWindowPagesViewModels/PatientInfoPageViewModel.cs
+++ MedicalCentre/ViewModels/DoctorWindowPagesViewModels/PatientInfoPageViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using MedicalCentre.Models;
 using MedicalCentre.Pages.DoctorWindowPages;
-using MedicalCentre.Services;
 using System.Windows.Input;
 
 namespace MedicalCentre.ViewModels.DoctorWindowPagesViewModels
@@ -10,16 +9,17 @@ namespace MedicalCentre.ViewModels.DoctorWindowPagesViewModels
         private readonly PatientInfoPage page;
         public Patient CurrentPatient { get; set; }
         public ICommand ViewPatientsNotesCommand { get; set; }
+        public ICommand ViewPatientsExaminationsCommand { get; set; }
 
         public PatientInfoPageViewModel(Patient patient, PatientInfoPage page)
         {
             CurrentPatient = patient;
             this.page = page;
             ViewPatientsNotesCommand = new RelayCommand(ViewPatientsNotes);
+            ViewPatientsExaminationsCommand = new RelayCommand(ViewPatientExaminations);
         }
-        private void ViewPatientsNotes()
-        {
-            page.LeftFrame.Content = new PatientsNotesPage(CurrentPatient.Notes);
-        }
+        private void ViewPatientsNotes() => page.LeftFrame.Content = new PatientsNotesPage(CurrentPatient.Notes);
+
+        private void ViewPatientExaminations() => page.LeftFrame.Content = new PatientExaminationsPage(CurrentPatient.Examinations);
     }
 }
