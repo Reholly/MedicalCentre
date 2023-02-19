5f12c4de659f4e4b0cc10d383ceac7fc9d180311
Степан <@LAPTOP-B0PF4KGO>
2023-02-19 15:46:04 +0500

    Finished backend of Doctor's window (without refactoring)

---
+++ MedicalCentre/Pages/DoctorWindowPages/CreateExaminationPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MedicalCentre.Models;
+using MedicalCentre.ViewModels.DoctorWindowPagesViewModels;
+using System.Windows.Controls;
+
+namespace MedicalCentre.Pages.DoctorWindowPages
+{
+    public partial class CreateExaminationPage : Page
+    {
+        public MedicalExamination CreatedExamination { get; set; }
+        public bool IsCreated { get; set; } = false;
+        public Patient Patient { get; set; }
+        public CreateExaminationPage(Patient patient)
+        {
+            InitializeComponent();
+            DataContext = new CreateExaminationPageViewModel(this);
+            Patient = patient;
+        }
+    }
+}
--- MedicalCentre/ViewModels/DoctorWindowPagesViewModels/PatientInfoPageViewModel.cs
+++ MedicalCentre/ViewModels/DoctorWindowPagesViewModels/PatientInfoPageViewModel.cs
@@ -10,6 +10,8 @@ namespace MedicalCentre.ViewModels.DoctorWindowPagesViewModels
         public Patient CurrentPatient { get; set; }
         public ICommand ViewPatientsNotesCommand { get; set; }
         public ICommand ViewPatientsExaminationsCommand { get; set; }
+        public ICommand CreateNoteCommand { get; set; }
+        public ICommand CreateExaminationCommand { get; set; }
 
         public PatientInfoPageViewModel(Patient patient, PatientInfoPage page)
         {
@@ -17,9 +19,30 @@ namespace MedicalCentre.ViewModels.DoctorWindowPagesViewModels
             this.page = page;
             ViewPatientsNotesCommand = new RelayCommand(ViewPatientsNotes);
             ViewPatientsExaminationsCommand = new RelayCommand(ViewPatientExaminations);
+            CreateNoteCommand = new RelayCommand(CreateNote);
+            CreateExaminationCommand = new RelayCommand(CreateExamination);
         }
+
         private void ViewPatientsNotes() => page.LeftFrame.Content = new PatientsNotesPage(CurrentPatient.Notes);
 
         private void ViewPatientExaminations() => page.LeftFrame.Content = new PatientExaminationsPage(CurrentPatient.Examinations);
+        private void CreateNote()
+        {
+            CreateNotePage notePage = new();
+            page.RightFrame.Content = notePage;
+            Note note = new();
+            if (notePage.IsCreated)
+                note = notePage.CreatedNote;
+            CurrentPatient.Notes.Add(note);
+        }
+        private void CreateExamination()
+        {
+            CreateExaminationPage Page = new(CurrentPatient);
+            page.RightFrame.Content = Page;
+            MedicalExamination examination = new();
+            if (Page.IsCreated)
+                examination = Page.CreatedExamination;
+            CurrentPatient.Examinations.Add(examination);
+        }
     }
 }
