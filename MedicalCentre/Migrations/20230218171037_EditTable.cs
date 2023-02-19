590a97194103c4d47237b2d632006a4ac75e0b9d
EuUst <atokadota2@yandex.ru>
2023-02-19 10:32:38 +0500

    refactor

---
+++ MedicalCentre/Migrations/20230218171037_EditTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicalCentre.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Roles_RoleId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_RoleId",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFinished",
+                table: "MedicalExaminations",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFinished",
+                table: "MedicalExaminations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_RoleId",
+                table: "Employees",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Roles_RoleId",
+                table: "Employees",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
