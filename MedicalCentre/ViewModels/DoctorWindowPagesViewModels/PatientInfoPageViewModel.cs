90f9b057d377ae3cc5842130949f1d44c5604fff
Степан <@LAPTOP-B0PF4KGO>
2023-02-19 11:13:03 +0500

    Added Patient's Notes Page

--- MedicalCentre/ViewModels/DoctorWindowPagesViewModels/PatientInfoPageViewModel.cs
+++ MedicalCentre/ViewModels/DoctorWindowPagesViewModels/PatientInfoPageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using MedicalCentre.Models;
 using MedicalCentre.Pages.DoctorWindowPages;
+using MedicalCentre.Services;
+using System.Windows.Input;
 
 namespace MedicalCentre.ViewModels.DoctorWindowPagesViewModels
 {
@@ -7,11 +9,17 @@ namespace MedicalCentre.ViewModels.DoctorWindowPagesViewModels
     {
         private readonly PatientInfoPage page;
         public Patient CurrentPatient { get; set; }
+        public ICommand ViewPatientsNotesCommand { get; set; }
 
         public PatientInfoPageViewModel(Patient patient, PatientInfoPage page)
         {
             CurrentPatient = patient;
             this.page = page;
+            ViewPatientsNotesCommand = new RelayCommand(ViewPatientsNotes);
+        }
+        private void ViewPatientsNotes()
+        {
+            page.LeftFrame.Content = new PatientsNotesPage(CurrentPatient.Notes);
         }
     }
 }
