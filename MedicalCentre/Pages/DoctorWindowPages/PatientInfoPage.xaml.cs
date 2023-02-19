054e29b2b80f48e2b0bc81777c39945ea9dc51aa
Степан <@LAPTOP-B0PF4KGO>
2023-02-19 10:43:00 +0500

    Added PatientInfoPage and it's ViewModel

---
+++ MedicalCentre/Pages/DoctorWindowPages/PatientInfoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using MedicalCentre.Models;
+using MedicalCentre.ViewModels.DoctorWindowPagesViewModels;
+
+namespace MedicalCentre.Pages.DoctorWindowPages
+{
+    public partial class PatientInfoPage : Page
+    {
+        public PatientInfoPage(Patient patinet)
+        {
+            InitializeComponent();
+            DataContext = new PatientInfoPageViewModel(patinet, this);
+        }
+    }
+}
