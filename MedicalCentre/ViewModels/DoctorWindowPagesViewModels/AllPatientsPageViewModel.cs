9874330af88706fe033453da8a28571a9b422a87
Степан <@LAPTOP-B0PF4KGO>
2023-02-19 10:43:00 +0500

    Added PatientInfoPage and it's ViewModel

--- MedicalCentre/ViewModels/DoctorWindowPagesViewModels/AllPatientsPageViewModel.cs
+++ MedicalCentre/ViewModels/DoctorWindowPagesViewModels/AllPatientsPageViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Navigation;
 using MedicalCentre.Models;
 using MedicalCentre.Services;
 
@@ -15,11 +16,22 @@ namespace MedicalCentre.ViewModels.DoctorWindowPagesViewModels
         public ObservableCollection<Patient> Patients { get; set; } = new();
         public Patient SelectedPatient { get; set; }
         public ICommand AddPatientCommand { get; set; }
+        public ICommand DeletePatientCommand { get; set; }
+        public ICommand ShowPatientInfoCommand { get; set; }
         public AllPatientsPageViewModel()
         {
             AddPatientCommand = new RelayCommand(AddPatient);
+            DeletePatientCommand = new RelayCommand(DeletePatient);
+            ShowPatientInfoCommand = new RelayCommand(ShowPatientInfo);
         }
 
         private void AddPatient() => Patients.Add(new Patient());
+        private void DeletePatient() => Patients.Remove(SelectedPatient);
+        private void ShowPatientInfo()
+        {
+            NavigationWindow window = new NavigationWindow();
+            window.Content = new object();
+            window.Show();
+        }
     }
 }
