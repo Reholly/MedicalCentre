a003dbfea4db22093ed7a6b570aff0ebd5f0b045
Степан <@LAPTOP-B0PF4KGO>
2023-02-19 11:13:03 +0500

    Added Patient's Notes Page

---
+++ MedicalCentre/ViewModels/DoctorWindowPagesViewModels/PatientNotesPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using MedicalCentre.Models;
+
+namespace MedicalCentre.ViewModels.DoctorWindowPagesViewModels
+{
+    public class PatientNotesPageViewModel
+    {
+        public ObservableCollection<Note> Notes { get; set; }
+        public PatientNotesPageViewModel(IEnumerable<Note> notes)
+        {
+            Notes = (ObservableCollection<Note>)notes;
+        }
+    }
+}
