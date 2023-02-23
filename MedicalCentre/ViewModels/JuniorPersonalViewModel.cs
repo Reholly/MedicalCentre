be9a61496985d3e67ef89036b33d8f90f5a864f8
Степан <@LAPTOP-B0PF4KGO>
2023-02-23 10:59:49 +0500

    Refactoring + add junior personal window

---
+++ MedicalCentre/ViewModels/JuniorPersonalViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MedicalCentre.Pages.JuniorPersonalWindowPages;
+using MedicalCentre.ViewModels.JuniorPersonalWindowPagesViewModels;
+using MedicalCentre.Windows;
+using System.Windows.Input;
+
+namespace MedicalCentre.ViewModels
+{
+    public class JuniorPersonalViewModel
+    {
+        private JuniorPersonalWindow window;
+        public ICommand ShowStorageItemsCommand { get; set; }
+        public JuniorPersonalViewModel(JuniorPersonalWindow window)
+        {
+            this.window = window;
+            ShowStorageItemsCommand = new RelayCommand(ShowStorageItems);
+        }
+
+        private void ShowStorageItems() => window.frame.Content = new StorageItemsPage();
+    }
+}
