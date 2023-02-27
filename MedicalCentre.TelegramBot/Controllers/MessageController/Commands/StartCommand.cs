e3a60510802c394009cf85866fceafe793e057d3
Kharinov Dmitry <web-giorno@mail.ru>
2023-02-27 19:55:21 +0500

    test commit

--- MedicalCentre.TelegramBot/Controllers/MessageController/Commands/StartCommand.cs
+++ MedicalCentre.TelegramBot/Controllers/MessageController/Commands/StartCommand.cs
@@ -1,18 +1,23 @@
 ﻿using MedicalCentre.TelegramBot.Models;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace MedicalCentre.TelegramBot.Controllers.MessageController.Commands
 {
     internal class StartCommand : Command
     {
+        private string hello = "Вас приветствует Medical Centre Bot!\n" +
+                               "Для начала работы зарегестрируйтесь или авторизуйтесь!";
+
         protected override TelegramBotClient client => Bot.GetTelegramBot();
 
         public override string Name => "/start";
 
         public override void Execute(Update update)
         {
-            client.SendTextMessageAsync(update.Message.Chat.Id, "Вас приветствует Medical Centre Bot! На ротан дать?");
+            KeyboardButton regster = new KeyboardButton("Регистрация");
+            client.SendTextMessageAsync(update.Message.Chat.Id, hello);
         }
     }
 }
