57a200ff65264dd6f34dc0b01720b9b9c07240ed
Kharinov Dmitry <web-giorno@mail.ru>
2023-03-02 19:43:37 +0500

    appointment done

--- MedicalCentre.TelegramBot/Controllers/MessageController/Commands/AppointmentCommand.cs
+++ MedicalCentre.TelegramBot/Controllers/MessageController/Commands/AppointmentCommand.cs
@@ -1,4 +1,7 @@
-﻿using MedicalCentre.TelegramBot.Models;
+﻿using MedicalCentre.DatabaseLayer;
+using MedicalCentre.Models;
+using MedicalCentre.TelegramBot.DataBaseLayer;
+using MedicalCentre.TelegramBot.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using User = MedicalCentre.TelegramBot.Models.User;
 
 namespace MedicalCentre.TelegramBot.Controllers.MessageController.Commands
 {
@@ -22,16 +27,103 @@ namespace MedicalCentre.TelegramBot.Controllers.MessageController.Commands
             Executor = executor;
         }
 
+        private string? doctorSpecialization = null;
+        private string? doctorName = null;
+        private string? appointmentTime = null;
+
+        private List<Appointment> appointments;
+
         public override void Execute(Update update)
         {
             Message msg = update.Message;
             long chatId = msg.Chat.Id;
             client.SendTextMessageAsync(chatId, "");
+
+            User? user = DatabaseTelegram.Users.Find(x => x.ChatId == chatId);
+            if (user == null)
+            {
+                KeyboardButton regsterBtn = new KeyboardButton("Регистрация");
+                var regsterMarkup = new ReplyKeyboardMarkup(regsterBtn)
+                {
+                    ResizeKeyboard = true,
+                    OneTimeKeyboard = true
+                };
+                client.SendTextMessageAsync(update.Message.Chat.Id, "Для начала работы зарегестрируйтесь или авторизуйтесь!", replyMarkup: regsterMarkup);
+                return;
+            }
+
+            Database<Appointment> db = new Database<Appointment>();
+            appointments = (List<Appointment>)db.GetTable().Where(x => x.Patient == null);
+
+            var specializationsBtn = appointments.Select(x => x.Doctor.Specialization)
+                                                 .Distinct()
+                                                 .Select(x => new KeyboardButton(x))
+                                                 .ToArray();
+
+            var specializationMarkup = new ReplyKeyboardMarkup(specializationsBtn)
+            {
+                ResizeKeyboard = true,
+                OneTimeKeyboard = true
+            };
+            client.SendTextMessageAsync(update.Message.Chat.Id, "Выеберите специальность врача", replyMarkup: specializationMarkup);
+
         }
 
         public void GetUpdate(Update update)
         {
-            throw new NotImplementedException();
+            if(doctorSpecialization == null)
+            {
+                doctorSpecialization = update.Message.Text;
+                var appointmentsTmp = appointments.Where(x => x.Doctor.Specialization == doctorSpecialization).ToList();
+                if(appointmentsTmp.Count == 0)
+                {
+                    doctorSpecialization = null;
+                    return;
+                }
+                appointments = appointmentsTmp;
+
+                var specializationBtn = appointments.Select(x => new KeyboardButton(x.Doctor.Name)).ToArray();
+                var specializationMarkup = new ReplyKeyboardMarkup(specializationBtn)
+                {
+                    ResizeKeyboard = true,
+                    OneTimeKeyboard = true
+                };
+                client.SendTextMessageAsync(update.Message.Chat.Id, "Выеберите врача", replyMarkup: specializationMarkup);
+            }
+            else if(doctorName == null)
+            {
+                doctorName = update.Message.Text;
+                var appointmentsTmp = appointments.Where(x => x.Doctor.Name == doctorName).ToList();
+                if (appointmentsTmp.Count == 0)
+                {
+                    doctorName = null;
+                    return;
+                }
+                appointments = appointmentsTmp;
+
+                var doctorsBtn = appointments.Select(x => new KeyboardButton(x.AppointmentTime.ToString())).ToArray();
+                var doctorsMarkup = new ReplyKeyboardMarkup(doctorsBtn)
+                {
+                    ResizeKeyboard = true,
+                    OneTimeKeyboard = true
+                };
+                client.SendTextMessageAsync(update.Message.Chat.Id, "Выеберите время", replyMarkup: doctorsMarkup);
+            }
+            else
+            {
+                appointmentTime = update.Message.Text;
+                Appointment? appointment = appointments.Find(x => x.AppointmentTime.ToString() == appointmentTime);
+                if(appointment == null)
+                {
+                    appointmentTime = null;
+                    return;
+                }
+                Database<Appointment> db = new Database<Appointment>();
+                db.UpdateItemAsync(appointment);
+
+                client.SendTextMessageAsync(update.Message.Chat.Id, "Вы успешно записанны");
+                Executor.StopListen();
+            }
         }
     }
 }
