73f0b57287af96c641b5c1959a03c3e496da909d
Степан <@LAPTOP-B0PF4KGO>
2023-03-09 15:10:47 +0500

    Doctor window refactoring 1

--- MedicalCentre/Pages/DoctorWindowPages/CreateExaminationPage.xaml.cs
+++ MedicalCentre/Pages/DoctorWindowPages/CreateExaminationPage.xaml.cs
@@ -7,13 +7,11 @@ namespace MedicalCentre.Pages.DoctorWindowPages
     public partial class CreateExaminationPage : Page
     {
         public MedicalExamination CreatedExamination { get; set; }
-        public bool IsCreated { get; set; } = false;
         public Patient Patient { get; set; }
         public CreateExaminationPage(Patient patient)
         {
             InitializeComponent();
-            DataContext = new CreateExaminationPageViewModel(this);
-            Patient = patient;
+            DataContext = new CreateExaminationPageViewModel(this, patient);
         }
     }
 }
