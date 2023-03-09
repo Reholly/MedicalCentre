5f2784674c06e37636c59dddced26f0a14fe22e7
EuUst <atokadota2@yandex.ru>
2023-03-09 10:10:58 +0500

    Auth - changed complete AdminWindow refactor - 50%

---
+++ MedicalCentre/UserControls/ViewModels/HumanCardViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MedicalCentre.Forms;
+using MedicalCentre.Models;
+using MedicalCentre.ViewModels;
+using System.Windows.Input;
+
+namespace MedicalCentre.UserControls.ViewModels
+{
+    internal class HumanCardViewModel
+    {
+        public ICommand? ProfileCommand { get; set; }
+        private Employee currentEmployee;
+        public HumanCardViewModel(HumanCard card, Employee employee)
+        {
+            currentEmployee = employee;
+            ProfileCommand = new RelayCommand(OpenProfile);
+            card.Card.Text = employee.ToString();
+        }
+        private void OpenProfile()
+        {
+            EmployeeProfile profile = new(currentEmployee);
+            profile.Show();
+        }
+    }
+}
