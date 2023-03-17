4970aa4cd0fc866a441e80bfe0aac78c4e38c72a
Степан <@LAPTOP-B0PF4KGO>
2023-03-17 12:03:47 +0500

    Start work with Doctor Window (again)

--- MedicalCentre/ViewModels/DoctorViewModel.cs
+++ MedicalCentre/ViewModels/DoctorViewModel.cs
@@ -11,28 +11,14 @@ namespace MedicalCentre.ViewModels
     {
         private readonly DoctorWindow window;
         private readonly Account account;
-        public ICommand ShowInputHelpCommand { get; set; }
-        public ICommand ShowTodaysAppointmentsCommand { get; set; }
-        public ICommand ShowAllPatientsCommand { get; set; }
         public ICommand WindowClosingCommand { get; set; }
 
         public DoctorViewModel(DoctorWindow window, Account account)
         {
             this.window = window;
             this.account = account;
-            ShowInputHelpCommand = new RelayCommand(ShowInputHelp);
-            ShowTodaysAppointmentsCommand = new RelayCommand(ShowTodaysAppointments);
-            ShowAllPatientsCommand = new RelayCommand(ShowAllPatients);
             WindowClosingCommand = new RelayCommand(Close);
         }
-
-        private void ShowInputHelp() => MessageBox.Show("DateTime input foramt: MM/DD/YYYY HH:MM:SS AM (or PM)");
-
-        private void ShowTodaysAppointments() => window.MainFrame.Content = new TodaysAppointments(window);
-        //показать список приёмов на сегодня
-
-        private void ShowAllPatients() => window.MainFrame.Content = new AllPatientsPage();
-        //показать список всех пациентов
 
         private void Close() //метод для LogOut кнопки
         {
