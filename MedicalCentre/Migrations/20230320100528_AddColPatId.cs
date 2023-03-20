98ca0a048cc91118abff901deca80c2f344d2da8
EuUst <atokadota2@yandex.ru>
2023-03-20 15:06:23 +0500

    Refact

---
+++ MedicalCentre/Migrations/20230320100528_AddColPatId.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicalCentre.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColPatId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Patients_PatientId",
+                table: "Notes");
+
+            migrationBuilder.AlterColumn<uint>(
+                name: "PatientId",
+                table: "Notes",
+                type: "int unsigned",
+                nullable: false,
+                defaultValue: 0u,
+                oldClrType: typeof(uint),
+                oldType: "int unsigned",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Patients_PatientId",
+                table: "Notes",
+                column: "PatientId",
+                principalTable: "Patients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Patients_PatientId",
+                table: "Notes");
+
+            migrationBuilder.AlterColumn<uint>(
+                name: "PatientId",
+                table: "Notes",
+                type: "int unsigned",
+                nullable: true,
+                oldClrType: typeof(uint),
+                oldType: "int unsigned");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Patients_PatientId",
+                table: "Notes",
+                column: "PatientId",
+                principalTable: "Patients",
+                principalColumn: "Id");
+        }
+    }
+}
--- MedicalCentre/Models/Note.cs
+++ MedicalCentre/Models/Note.cs
@@ -7,22 +7,25 @@ namespace MedicalCentre.Models
     public class Note : INotifyPropertyChanged
     {
         public uint Id { get; set; }
+        public uint PatientId { get; set; }
         public string Title { get; set; } = null!;
         public string NoteText { get; set; } = null!;
         public DateTime PublicationDate { get; set; }
 
         public Note() { }
 
-        public Note(string title, string noteText, DateTime publicationDate)
+        public Note(uint patientId, string title, string noteText, DateTime publicationDate)
         {
             Title = title;
+            PatientId = patientId;
             NoteText = noteText;
             PublicationDate = publicationDate;
         }
 
-        public Note(uint id, string title, string noteText, DateTime publicationDate)
+        public Note(uint id, uint patientId, string title, string noteText, DateTime publicationDate)
         {
             Id = id;
+            PatientId = patientId;
             Title = title;
             NoteText = noteText;
             PublicationDate = publicationDate;
