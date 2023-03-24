67e1d543a6be1cae8aad5b0c2479a65e0122c8b6
EuUst <atokadota2@yandex.ru>
2023-03-24 13:32:18 +0500

    done jun med

---
+++ MedicalCentre/Migrations/20230324083023_DeleteImageData.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicalCentre.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteImageData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MedicalExaminations_Images_AttachedImageId",
+                table: "MedicalExaminations");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MedicalExaminations_AttachedImageId",
+                table: "MedicalExaminations");
+
+            migrationBuilder.DropColumn(
+                name: "AttachedImageId",
+                table: "MedicalExaminations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<uint>(
+                name: "AttachedImageId",
+                table: "MedicalExaminations",
+                type: "int unsigned",
+                nullable: false,
+                defaultValue: 0u);
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "int unsigned", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ImageBytes = table.Column<byte[]>(type: "longblob", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicalExaminations_AttachedImageId",
+                table: "MedicalExaminations",
+                column: "AttachedImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MedicalExaminations_Images_AttachedImageId",
+                table: "MedicalExaminations",
+                column: "AttachedImageId",
+                principalTable: "Images",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
