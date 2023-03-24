1e4ccec72d93fec76e6fe3efb6a84d762ebe6891
Степан <@LAPTOP-B0PF4KGO>
2023-03-24 12:30:01 +0500

    .

---
+++ MedicalCentre/Forms/ViewModels/CreateExaminationFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MedicalCentre.Forms.ViewModels
+{
+    public class CreateExaminationFormViewModel
+    {
+        private readonly CreateExaminationForm form;
+        public ICommand ChangesSavingCommand { get; set; }
+        public CreateExaminationFormViewModel(CreateExaminationForm form)
+        {
+            this.form = form;
+        }
+
+        private async Task SaveChanges()
+        {
+
+        }
+    }
+}
